748f74213b03416caeca0eb5dafef98cad3a96d5
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2023-04-11 16:04:15 +0500

    First Commit

--- src/components/Home/Rendered.tsx
+++ src/components/Home/Rendered.tsx
@@ -1,10 +1,12 @@
-import { Image, PockemonCart, Subtitle, Title, Types } from '@/src/components/Home/home.style'
+import { Image, PockemonCart, Subtitle, Title, Types, ImageWrapper } from '@/src/components/Home/home.style'
 import React from 'react'
 
 const Rendered = ({data}: any) => {
     return data?.pokemons?.map(({id, name, types, image}: any) =>
         <PockemonCart key={id}>
-            <Image src={image} alt="images"/>
+            <ImageWrapper>
+                <Image src={image} alt="images"/>
+            </ImageWrapper>
             <Title>{name}</Title>
             <Types>
                 {types.map((type: string, index: number) =>
--- src/components/Home/home.style.tsx
+++ src/components/Home/home.style.tsx
@@ -5,12 +5,12 @@ export const HomeComponent = styled.div`
   max-width: 1220px;
   margin: 50px auto;
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
   grid-gap: 20px;
   align-items: center;
 `
 export const PockemonCart = styled.div`
-  max-width: 340px;
+  max-width: 250px;
   height: auto;
 `
 export const Title = styled.h1`
@@ -23,7 +23,6 @@ export const Types = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: center;
   gap: 30px;
 `
 
@@ -31,6 +30,18 @@ export const Subtitle = styled.p`
   font-size: 12px;
   line-height: 16px;
 `
+export const ImageWrapper = styled.div`
+  width: 250px;
+  height: 200px;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-radius: 8px;
+`
 export const Image = styled.img`
-  max-width: 350px;
+  width: 150px;
+  height: 150px;
+  object-fit: contain;
 `
