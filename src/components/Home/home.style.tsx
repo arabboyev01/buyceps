eea105697dd84de02b055631dee0fe0deeda87e7
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2023-04-11 18:45:47 +0500

    First Commit

--- src/components/Home/home.style.tsx
+++ src/components/Home/home.style.tsx
@@ -2,7 +2,7 @@
 import styled from 'styled-components'
 
 export const HomeComponent = styled.div`
-  max-width: 1220px;
+  max-width: 1020px;
   margin: 50px auto;
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(220px, 1fr));
@@ -14,6 +14,12 @@ export const PockemonCart = styled.div`
    border: 1px solid rgba(0, 0, 0, 0.1);
   border-radius: 8px;
   padding: 10px;
+  cursor: pointer;
+  transition: .3s ease-in-out;
+  &:hover{
+    transform: scale(1.02);
+    box-shadow: 1px 1px 6px 0.5px rgba(0, 0, 0, 0.1);
+  }
 `
 export const Title = styled.h1`
   font-size: 22px;
--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -3,14 +3,14 @@ import { Puff } from 'react-loader-spinner'
 const Loader = () => {
     return (
         <Puff
-            height="80"
-            width="80"
+            height="50"
+            width="50"
             radius={1}
             color="#ccc"
             ariaLabel="puff-loading"
             wrapperClass=""
             visible={true}
-            wrapperStyle={{position: 'absolute', top: '45%', left: "47%"}}
+            wrapperStyle={{position: 'absolute', top: '45%', left: "48%"}}
         />
     )
 }
