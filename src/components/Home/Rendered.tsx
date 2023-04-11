0e44d4e63489b413ef6852c714feff5aae34a1d6
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2023-04-11 16:11:50 +0500

    First Commit

--- src/components/Home/Rendered.tsx
+++ src/components/Home/Rendered.tsx
@@ -1,12 +1,13 @@
-import { Image, PockemonCart, Subtitle, Title, Types, ImageWrapper } from '@/src/components/Home/home.style'
+import { Image, PockemonCart, Subtitle, Title, Types, ImageWrapper, Number } from '@/src/components/Home/home.style'
 import React from 'react'
 
 const Rendered = ({data}: any) => {
-    return data?.pokemons?.map(({id, name, types, image}: any) =>
+    return data?.pokemons?.map(({id, name, types, image, number}: any) =>
         <PockemonCart key={id}>
             <ImageWrapper>
                 <Image src={image} alt="images"/>
             </ImageWrapper>
+            <Number>#{number}</Number>
             <Title>{name}</Title>
             <Types>
                 {types.map((type: string, index: number) =>
--- src/components/Home/home.style.tsx
+++ src/components/Home/home.style.tsx
@@ -6,18 +6,21 @@ export const HomeComponent = styled.div`
   margin: 50px auto;
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-  grid-gap: 20px;
+  grid-row-gap: 60px;
   align-items: center;
 `
 export const PockemonCart = styled.div`
   max-width: 250px;
   height: auto;
+   border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+  padding: 10px;
 `
 export const Title = styled.h1`
-  font-size: 18px;
+  font-size: 22px;
   margin: 18px 0;
   font-weight: 700;
-  color: darkorange;
+  color: #000;
 `
 export const Types = styled.div`
   display: flex;
@@ -33,15 +36,19 @@ export const Subtitle = styled.p`
 export const ImageWrapper = styled.div`
   width: 250px;
   height: 200px;
-  border: 1px solid rgba(0, 0, 0, 0.1);
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  border-radius: 8px;
 `
 export const Image = styled.img`
   width: 150px;
   height: 150px;
   object-fit: contain;
+`
+export const Number = styled.p`
+ margin-top: 5px;
+  font-size: 22px;
+  font-weight: 700;
+  color: rgba(0, 0, 0, 0.4);
 `
